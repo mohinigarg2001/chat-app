7776f9c2b6dbdf653405a7f92937650a74ccb1f0
Mohini <82579310+mohinigarg2001@users.noreply.github.com>
2021-08-06 12:42:45 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app= express();
+const http = require('http');
+const path=require('path');
+const socketio= require('socket.io');
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.use('/',express.static(path.join(__dirname,'/public')));
+
+const users={}
+
+io.on('connection',(socket)=>{
+
+    socket.on('login',(data)=>{
+        users[socket.id]=data.name
+    })
+
+    socket.on('send_msg',(data)=>{
+        
+        io.emit('received_msg',{
+            msg:data.msg,
+            name:users[socket.id]
+        })
+    })
+})
+
+
+server.listen(process.env.PORT || 3001,()=>{
+    console.log('server is running at http://localhost:3001 ');
+})
---
+++ public/script.js
@@ -0,0 +1,29 @@
+const socket = io();
+
+$('#chat').hide()
+
+
+$('#login-btn').click(()=>{
+    socket.emit('login',{
+        name:$('#login-inp').val()
+    })
+
+    $('#login').hide();
+    $('#chat').show();
+
+})
+$('#send-btn').click(()=>{
+    socket.emit('send_msg',{
+        msg:$('#inp').val()
+    
+    })
+    $('#inp').val("")
+    $('.emojionearea-editor').text("")
+})
+
+socket.on('received_msg',(data)=>{
+    $('#list').append(`<li> <strong>${data.name}</strong>:${data.msg}</li>`)
+    
+})
+
+
